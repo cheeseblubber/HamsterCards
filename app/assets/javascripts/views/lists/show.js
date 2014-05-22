a5063e2064aa84ced7f682228ef047a968ff7fa3
App Academy Student <appacademy@Apps-Mac-mini-447.local>
2014-05-21 19:35:26 -0700

    refactored board show moving add form to own method

--- app/assets/javascripts/views/lists/show.js
+++ app/assets/javascripts/views/lists/show.js
@@ -8,24 +8,20 @@ Trello.Views.ListItem = Backbone.CompositeView.extend({
     // this.listenTo(
     //   this.model.cards(), "add", this.addCard
     // );
-    // var cardNewView = new Trello.Views.CardItem({ model: this.model });
-    // this.addSubview("card-list", cardNewView);
+    var cardNewView = new Trello.Views.CardItem({ model: this.model });
+    this.addSubview("card-list", cardNewView);
     // this.model.cards().each(this.addCard.bind(this));
   },
 
-  // addCard: function (card) {
-  //   var cardShow = new Trello.CardItem({ model: card });
-  //   this.addSubview("card-list", cardNewView);
-  // },
 
-
-  renderCards: function (list) {
-    var that = this
-    var cardCollection = new Trello.Collections.Cards([], { list: list })
-    cardCollection.fetch()
-    var cardShow = new Trello.Views.CardIndex({ collection: cardCollection})
-    that.addSubview(".cards-list", cardShow)
-  },
+	// move this to board class because of rerendering problems
+	  renderCards: function (list) {
+	    var that = this
+	    var cardCollection = new Trello.Collections.Cards([], { list: list })
+	    cardCollection.fetch()
+	    var cardShow = new Trello.Views.CardIndex({ collection: cardCollection})
+	    that.addSubview(".cards-list", cardShow)
+	  },
 
 	renderCardForm: function(list){
 		var that = this
@@ -39,6 +35,7 @@ Trello.Views.ListItem = Backbone.CompositeView.extend({
     });
 
     this.$el.html(content);
+		//move these to board class
 		this.renderCards(this.model);
 		this.renderCardForm(this.model);
     return this
