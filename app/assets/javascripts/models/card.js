d2a9f15b2724df45f20bd46ccd5d03fb1110269b
Kam Leung <Kamleung911@gmail.com>
2014-05-23 14:13:13 -0700

    Add list and remove list functioning

--- app/assets/javascripts/models/card.js
+++ app/assets/javascripts/models/card.js
@@ -1,8 +1,5 @@
 Trello.Models.Card = Backbone.Model.extend({
 
-	initialize: function () {
-		this.deleteUrl = "api/cards/" + this.id
-	},
 
   url: function () {
     return "api/lists/" + this.attributes.list_id + "/cards"
