1854f0c42b59a11a6ed80fa764c0abdab0387a43
Kam Leung <Kamleung911@gmail.com>
2014-05-23 11:41:14 -0700

    working delete button

--- app/assets/javascripts/views/card/show.js
+++ app/assets/javascripts/views/card/show.js
@@ -6,10 +6,35 @@ Trello.Views.CardItem = Backbone.View.extend({
 
 	className: 'card',
 
-  render: function () {
+	events: {
+		"mouseover": "showFeatures",
+		"mouseleave": "hideFeatures",
+		"click .float-right-button": "deleteCard",
+	},
+
+	render: function () {
     var content = this.template({ card: this.model });
     this.$el.html(content);
     return this;
-  }
+  },
+
+	showFeatures: function () {
+		//this is a view and has
+		//this.model which is the card and has the attribute list_id'
+		$('#'+ this.model.id).removeClass('hidden')
+	},
+
+	hideFeatures: function () {
+		$('#'+ this.model.id).addClass('hidden')
+	},
+
+	deleteCard: function () {
+		//hacked because of routes are different for different Create and delete
+    $.ajax({
+      url: "api/cards/" + this.model.id,
+      method: 'DELETE',
+    })
+		this.model.destroy()
+	},
 
 })
