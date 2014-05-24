ca7c29a4056b40dc665022227c62a0a6f209dbbf
Kam Leung <Kamleung911@gmail.com>
2014-05-24 13:50:16 -0700

    refactored add cards to not use index

--- app/assets/javascripts/views/lists/show.js
+++ app/assets/javascripts/views/lists/show.js
@@ -1,7 +1,7 @@
 Trello.Views.ListItem = Backbone.CompositeView.extend({
   tagName: 'li',
 
-	className: 'panel',
+	className: 'list panel',
 
 	idAttribute: function () {
 		return this.model.id
@@ -14,9 +14,13 @@ Trello.Views.ListItem = Backbone.CompositeView.extend({
 	},
 
 	initialize: function () {
+		// this.cards = this.model.cards()
 		// instead of rendering every time refactor this
 		// to use sync and subviews
-		this.listenTo(this.model.cards(), 'all', this.render)
+		this.listenTo(this.model.cards(), 'add', this.addCard)
+		// this.cards.fetch();
+		this.addCardForm(this.model);
+		// this.cards.each(this.addCard.bind(this));
 	},
 
 	deleteList: function () {
@@ -24,29 +28,46 @@ Trello.Views.ListItem = Backbone.CompositeView.extend({
 		this.remove();
 	},
 
+	addCard: function(list){
+		var cardView = new Trello.Views.CardItem({ model: list})
+		this.addSubview(".cards-list", cardView)
+	},
+
+
 	//refactor to remove index views
+  // renderCards: function (list) {
+  //   var that = this
+  //   var cardCollection = new Trello.Collections.Cards([], { list: list })
+  //   cardCollection.fetch()
+  // 		//problem is this fetch here the list doesn't have an id yet
+  //   var cardShow = new Trello.Views.CardIndex({ collection: cardCollection})
+  //   that.addSubview(".cards-list", cardShow)
+  // },
+
   renderCards: function (list) {
     var that = this
-    var cardCollection = new Trello.Collections.Cards([], { list: list })
+    var cardCollection = this.model.cards();
     cardCollection.fetch()
-		//problem is this fetch here the list doesn't have an id yet
-    var cardShow = new Trello.Views.CardIndex({ collection: cardCollection})
-    that.addSubview(".cards-list", cardShow)
+		cardCollection.each(this.addCard.bind(this))
+    // var cardShow = new Trello.Views.CardIndex({ collection: cardCollection})
+    // that.addSubview(".cards-list", cardShow)
   },
 
-	renderCardForm: function(list){
+
+	addCardForm: function(list){
 		var that = this
 		var cardFormView = new Trello.Views.CardForm({ list: list})
 		that.addSubview(".card-form", cardFormView)
 	},
 
   render: function () {
-		var content = this.template({
+		var renderedContent = this.template({
       list: this.model
     });
-    this.$el.html(content);
-		this.renderCards(this.model);
-		this.renderCardForm(this.model);
+    this.$el.html(renderedContent);
+		// debugger
+		this.renderCards(this.model)
+		this.attachSubviews();
     return this
   },
 
